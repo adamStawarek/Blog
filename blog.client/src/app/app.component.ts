bee0859a4631c866652c88850f5935c83011d5f3
Adam Stawarek <asta@netcompany.com>
2024-09-07 18:01:42 +0200

    Set up angular project

--- blog.client/src/app/app.component.ts
+++ blog.client/src/app/app.component.ts
@@ -11,12 +11,12 @@ interface WeatherForecast {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getForecasts();
--- blog.client/src/app/app.module.ts
+++ blog.client/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  bootstrap: [AppComponent],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule { }
