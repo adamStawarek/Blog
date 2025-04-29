fac0a3ec2beb1fb9220c4a98bea35432fa246e81
Adam Stawarek <adam.stawarek@gmail.com>
2025-04-30 00:35:30 +0200

    Fix job registration

--- src/Blog.Clients.Jobs/Program.cs
+++ src/Blog.Clients.Jobs/Program.cs
@@ -47,7 +47,9 @@ builder.Services.AddHostedService<JobsHostedService>();
 
 builder.Services.Scan(scan => scan
     .FromAssemblyOf<JobsHostedService>()
-    .AddClasses(classes => classes.AssignableTo<IBlogJob>())
+    .AddClasses(classes => classes
+        .AssignableTo<IBlogJob>()
+        .AssignableTo(typeof(IBlogJob<>)))
     .AsImplementedInterfaces()
     .WithScopedLifetime());
 
